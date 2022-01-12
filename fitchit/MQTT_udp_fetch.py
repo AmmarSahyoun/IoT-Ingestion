4d905f1d2b66e680400fa9405aa8fe97bd85e68e
Ammar sahyoun <ammarzgr@hotmail.com>
2022-01-12 15:08:51 +0100

    clean and test

--- fitchit/MQTT_udp_fetch.py
+++ fitchit/MQTT_udp_fetch.py
@@ -63,20 +63,59 @@ def saveToFile(json_file):
 		fw.writerow([received_at, f_port, f_cnt, frm_payload, rssi, snr, data_rate_index, consumed_airtime])
 
 
-# MQTT event functions
+# MQTT events
 def on_connect(mqttc, obj, flags, rc):
 	print("\nConnect: rc = " + str(rc))
 
-def on_message(mqttc, obj, msg):
-    print("\nMessage: " + msg.topic + " " + str(msg.qos)) # + " " + str(msg.payload))
-    parsedJSON = json.loads(msg.payload)
-    #print(json.dumps(parsedJSON, indent=4))	# Uncomment this to fill your terminal screen with JSON
-    saveToFile(parsedJSON)
-
 def on_subscribe(mqttc, obj, mid, granted_qos):
-    print("\nSubscribe: " + str(mid) + " " + str(granted_qos))
+	print("\nSubscribe: " + str(mid) + " " + str(granted_qos))
+
+def on_message(mqttc, obj, msg):
+	print("\nMessage: " + msg.topic + " " + str(msg.qos)) # + " " + str(msg.payload))
+	parsedJSON = json.loads(msg.payload)
+	#print(json.dumps(parsedJSON, indent=4))	# Uncomment this to fill your terminal screen with JSON
+	saveToFile(parsedJSON)
 
 def on_log(mqttc, obj, level, string):
-    print("\nLog: "+ string)
-    logging_level = mqtt.LOGGING_LEVEL[level]
-    logging.log(logging_level, string)
+	print("\nLog: "+ string)
+	logging_level = mqtt.LOGGING_LEVEL[level]
+	logging.log(logging_level, string)
+
+
+
+def main():
+	mqttc = mqtt.Client()
+	mqttc.on_connect = on_connect
+	mqttc.on_subscribe = on_subscribe
+	mqttc.on_message = on_message
+	# mqttc.on_log = on_log		# Logging for debugging
+	print("Connect")
+
+	mqttc.username_pw_set(User, Password)
+
+	# IMPORTANT - this enables the encryption of messages
+	mqttc.tls_set()  # default certification authority of the system
+
+	# mqttc.tls_set(ca_certs="mqtt-ca.pem") # Use this if you get security errors
+	# It loads the TTI security certificate. Download it from their website from this page:
+	# https://www.thethingsnetwork.org/docs/applications/mqtt/api/index.html
+	# This is normally required if you are running the script on Windows!
+
+	mqttc.connect(theRegion.lower() + ".cloud.thethings.network", 8883, 60)
+
+	print("Subscribe")
+	mqttc.subscribe("#", 0)
+	print("And run forever")
+	try:
+		run = True
+		while run:
+			mqttc.loop(10)  # seconds timeout / blocking time
+			print(".", end="", flush=True)  # feedback to the user that something is actually happening
+
+	except KeyboardInterrupt:
+		print("Exit")
+		sys.exit(0)
+
+
+if __name__ == '__main__':
+	main()
